244a909d5f3c0a14647897f243a4c76c1f0bf9c5
Timothy <TimothyOLeary001@gmail.com>
2020-01-23 11:32:57 -0500

    Added comment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
+        //example comment 
         System.out.println("Hello Comp 152");
+        System.out.println("Let's make it works");
     }
 }
